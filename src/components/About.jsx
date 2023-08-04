f3e8f7df160e6b669ead388799f743b0fc4ddb85
Rahul Mallick <rahulmallick1126@gmail.com>
2023-08-04 15:56:07 +0530

    changed the background of body

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,16 @@ export const About = () => {
     <div className="bg-white">
       <div className="p-24 grid grid-cols-2">
         <div className="">
-          <h2 className="text-2xl font-medium">About Us</h2>
-          <p className="text-lg">
+          <h2 className="text-2xl font-semibold">About Us</h2>
+          <p className="text-lg ">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quod
             sunt aut ad, officia voluptatem ullam culpa pariatur distinctio?
             Soluta doloremque voluptatem placeat quo aliquam repellat tenetur
             voluptates autem quam quae recusandae natus facere ipsum alias velit
-            voluptatum ducimus accusamus officiis rerum, quasi ea perferendis?
+            voluptatum ducimus accusamus officiis rerum, quasi ea
+            perferendis?Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Minima maiores id expedita. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Amet tenetur dicta minima!
           </p>
         </div>
         <div className="flex items-center justify-center">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <nav id="header" className="bg-black text-white">
-      <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+      <div className="w-full container mx-auto flex flex-wrap items-center justify-around mt-0 py-2">
         <div className="logo-wrapper flex pl-4 items-center">
           <Link
             to="/"
